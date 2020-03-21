61bbca5ff6eec4bab0b096384bed30feb4db99ce
mike-petrov <mis5116@yandex.ru>
2020-03-21 21:59:56 +0300

    add initial functions

--- api/app/api.py
+++ api/app/api.py
@@ -9,10 +9,35 @@ import time
 cluster = MongoClient("mongodb://admin:mikedroplet@67.205.189.192:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false", connect = False )
 db = cluster['hackuniversity2020']
 users = db['users']
+books = db['books']
+
+@app.route('/top', methods=['POST'])
+def save_token():
+	x = request.json
+
+	if users.find_one({'token': x['token']}):
+		return json.dumps({'error': 'already'})
+	else:
+		user_data = {
+			'id': users.count() + 1,
+			'token': users.count() + 1,
+			'search': [],
+		}
+		users.insert_one(user_data)
+
+		results = books.find({}).limit(3)
+		ans = []
+		for result in results:
+			ans.append(result)
+
+		return json.dumps(ans)
+
 
 @app.route('/search', methods=['POST'])
-def post():
-	results = users.find({})
+def search():
+	x = request.json
+
+	results = books.find({})
 	ans = []
 	for result in results:
 		ans.append(result)
