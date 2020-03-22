5b4496915ef9ac997c33b459f87e1d10b6ac20e6
mike-petrov <mis5116@yandex.ru>
2020-03-22 09:40:57 +0000

    working search function

--- api/app/api.py
+++ api/app/api.py
@@ -4,6 +4,8 @@ import pymongo
 from pymongo import MongoClient
 import json
 import time
+from gensim import corpora, models, similarities
+import jieba
 
 
 cluster = MongoClient("mongodb://admin:mikedroplet@67.205.189.192:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false", connect = False )
@@ -37,9 +39,26 @@ def save_token():
 @app.route('/search', methods=['POST'])
 def search():
 	x = request.json
-
-	results = books.find({},{'_id': False}).limit(3)
-	ans = []
+	results = list(books.find({}, {'_id': False}))
+	texts = []
 	for result in results:
-		ans.append(result)
-	return json.dumps(ans)
+		texts.append(result['description'])
+	keyword = x['cont']
+
+	texts = [jieba.lcut(text) for text in texts]
+	dictionary = corpora.Dictionary(texts)
+	feature_cnt = len(dictionary.token2id)
+	corpus = [dictionary.doc2bow(text) for text in texts]
+	tfidf = models.TfidfModel(corpus)
+	kw_vector = dictionary.doc2bow(jieba.lcut(keyword))
+	index = similarities.SparseMatrixSimilarity(tfidf[corpus], num_features = feature_cnt)
+	sim = index[tfidf[kw_vector]]
+
+	obj_with_sim = []
+	for i in range(len(sim)):
+		obj_with_sim.append([results[i], sim[i]])
+	obj_with_sim.sort(key = lambda text: text[1], reverse = True)
+	obj_with_sim = obj_with_sim[0: 10]
+	obj_with_sim = [t[0] for t in obj_with_sim]
+
+	return json.dumps(obj_with_sim)
